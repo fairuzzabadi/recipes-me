7843c39094fd644a846f36069ddb0d88c91dcc0f
fairuzzabadi <fairuzzabadi.279@gmail.com>
2019-03-20 09:20:20 +0700

    init push

---
+++ src/firebase/index.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+
+if(process.env.NODE_ENV === 'production') {
+    firebase.initializeApp(require('./keys_prod'));
+} else {
+    firebase.initializeApp(require('./keys_dev'));
+}
