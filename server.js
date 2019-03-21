602d25417f93aad4351948b87649f6a9b6b0508c
fairuzzabadi <fairuzzabadi.279@gmail.com>
2019-03-21 16:44:51 +0700

    Fix Heroku Deployment

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-
-const app = express()
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/'))
-
-app.use(staticFileMiddleware)
-app.use(history({
- disableDotRule: true,
- verbose: true
-}))
-app.use(staticFileMiddleware)
-
-app.get('/', function (req, res) {
- res.render(path.join(__dirname + '/dist/index.html'))
-})
-
-var server = app.listen(process.env.PORT || 8080, function () {
- var port = server.address().port
- console.log('App now running on port', port)
-})
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+// create the express app
+const app = express();
+// create middleware to handle the serving the app
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+// Catch all routes and redirect to the index file
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+// Create default port to serve the app on
+const port = process.env.PORT || 8080;
+app.listen(port);
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     firebase.initializeApp(require('./keys_prod'));
 } else {
     firebase.initializeApp(require('./keys_dev'));
