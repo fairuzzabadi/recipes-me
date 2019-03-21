e238814f908252deba7ac13bf52a4f5b7aaec0a4
fairuzzabadi <fairuzzabadi.279@gmail.com>
2019-03-21 15:26:15 +0700

    Fix Heroku Deployment

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
-// create the express app
+const history = require('connect-history-api-fallback')
+
 const app = express()
-// create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
-// Catch all routes and redirect to the index file
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history({
+ disableDotRule: true,
+ verbose: true
+}))
+app.use(staticFileMiddleware)
+
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+ res.render(path.join(__dirname + '/dist/index.html'))
 })
-// Create default port to serve the app on
-const port = 5000
-app.listen(port)
+
+var server = app.listen(process.env.PORT || 8080, function () {
+ var port = server.address().port
+ console.log('App now running on port', port)
+})
